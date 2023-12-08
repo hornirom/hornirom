43201a55eb0d06cf31ccf99c0f436fc45b3c06ee
Roman Horniichuk <hornirom@fit.cvut.cz>
2023-12-08 20:06:19 +0100

    hw00.c: added second version of program

--- homeWork00/hw00.c
+++ homeWork00/hw00.c
@@ -1,24 +1,23 @@
-#include <iostream>
+#include <stdio.h>
 
 int main ( int argc, char * argv []) {
     printf("ml' nob:\n");
     const char* arr[12] = {
-            "noH QapmeH wo' Qaw'lu'chugh yay chavbe'lu' 'ej wo' choqmeH may' DoHlu'chugh lujbe'lu'.\n",
-            "bortaS bIr jablu'DI' reH QaQqu' nay'.\n",
-            "Qu' buSHa'chugh SuvwI', batlhHa' vangchugh, qoj matlhHa'chugh, pagh ghaH SuvwI''e'.\n",
-            "bISeH'eghlaH'be'chugh latlh Dara'laH'be'.\n",
-            "qaStaHvIS wa' ram loS SaD Hugh SIjlaH qetbogh loD.\n",
-            "Suvlu'taHvIS yapbe' HoS neH.\n",
-            "Ha'DIbaH DaSop 'e' DaHechbe'chugh yIHoHQo'.\n",
-            "Heghlu'meH QaQ jajvam.\n",
-            "leghlaHchu'be'chugh mIn lo'laHbe' taj jej.\n",
+            "Qapla'\nnoH QapmeH wo' Qaw'lu'chugh yay chavbe'lu' 'ej wo' choqmeH may' DoHlu'chugh lujbe'lu'.\n",
+            "Qapla'\nbortaS bIr jablu'DI' reH QaQqu' nay'.\n",
+            "Qapla'\nQu' buSHa'chugh SuvwI', batlhHa' vangchugh, qoj matlhHa'chugh, pagh ghaH SuvwI''e'.\n",
+            "Qapla'\nbISeH'eghlaH'be'chugh latlh Dara'laH'be'.\n",
+            "Qapla'\nqaStaHvIS wa' ram loS SaD Hugh SIjlaH qetbogh loD.\n",
+            "Qapla'\nSuvlu'taHvIS yapbe' HoS neH.\n",
+            "Qapla'\nHa'DIbaH DaSop 'e' DaHechbe'chugh yIHoHQo'.\n",
+            "Qapla'\nHeghlu'meH QaQ jajvam.\n",
+            "Qapla'\nleghlaHchu'be'chugh mIn lo'laHbe' taj jej.\n",
             "Qih mi' %d\n",
             "Neh mi'\n",
             "bIjatlh 'e' yImev\n"
     };
     float number;
     if (scanf("%f", &number) == 1) {
-        printf("Qapla'\n");
         if (int(number) >= 0 and int(number) <= 8) {
             if (number != 0 and number != 1 and number != 2 and number != 3 and number != 4 and number != 5 and number != 6 and number != 7 and number != 8) {
                 printf("%s\n", arr[11]);
@@ -35,6 +34,6 @@ int main ( int argc, char * argv []) {
     }
     else printf("%s\n", arr[10]);
 
-    system ( "pause" );
+    //system ( "pause" );
     return 0;
 }
